df3b85853626d3838e9a6287e5ed88cf8222c87a
Pushkar <pushkarcricket@gmail.com>
2024-10-10 19:22:57 +0530

    code is pushed

--- Challenge-01/src/Counter.jsx
+++ Challenge-01/src/Counter.jsx
@@ -24,11 +24,19 @@ export default function Counter() {
     const handleStep2=()=>{
         setStep(step-1)
     }
+
+    const handleReset=()=>{
+        setStep("");
+        setCount("")
+    }
   return (
     <div>
         <h1> Welcome here</h1>
+
+        <input type="range" min={1} max={10}  value={step} onChange={(e)=>setStep(Number(e.target.value))}/>
         <div>
         <button onClick={handleStep2} >-</button>
+        {/* <input type='text' value={count} onChange={(e)=>setCount(Number(e.target.value))}/> */}
 
            <span><p>Step : {step}</p></span> 
            
@@ -43,11 +51,26 @@ export default function Counter() {
             </span>
 
 
-           
+           <div>
+
+
+           </div>
             
-            <button onClick={handleCount}>+</button>
+            
             
         </div>
+
+        {
+            count !== 0 || step !==1 ?(<div >
+                <button onClick={handleReset}>Reset</button>
+                </div>
+
+            ):null
+        }
+
+
+
+       
 
         
 <p>
